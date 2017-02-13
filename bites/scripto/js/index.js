53366ec50b8d21e94425c571cb52326d274651c3
Garrett Miller <gmiller@slack-corp.com>
2017-02-12 23:02:20 -0800

    fix up scripto logo

--- bites/scripto/js/index.js
+++ bites/scripto/js/index.js
@@ -6,6 +6,7 @@ var TxtType = function(el, toRotate, period) {
         this.txt = '';
         this.tick();
         this.isDeleting = false;
+        this.firstEver = false;
     };
 
     TxtType.prototype.tick = function() {
@@ -13,12 +14,22 @@ var TxtType = function(el, toRotate, period) {
         var fullTxt = this.toRotate[i];
 
         if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
         } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+        if(i > 0) {
+            this.firstEver = true;
         }
 
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+        if (this.firstEver && (this.txt === 'Sc' || this.txt === 'S' || this.txt === '')) {
+            this.el.innerHTML = '<span class="wrap">Scr</span>';
+        } else if(this.isDeleting && (this.txt === 'Sc' || this.txt === 'S' || this.txt === '')) {
+            this.el.innerHTML = '<span class="wrap">Scr</span>';
+            // debugger;
+        } else {
+            this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+        }
 
         var that = this;
         var delta = 200 - Math.random() * 100;
@@ -26,16 +37,32 @@ var TxtType = function(el, toRotate, period) {
         if (this.isDeleting) { delta /= 2; }
 
         if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
+            delta = this.period;
+            this.isDeleting = true;
         } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
         }
 
+        if(i === 1 && this.txt === 'Scripto') {
+            delta = 3000;
+        }
+
+        var cursor = document.querySelector(".wrap");
+        if(delta >= 500) {
+            cursor.classList.add('blink');
+            // make cursor blink
+        } else {
+            cursor.classList.remove('blink');
+        }
+
+        console.log('delta', delta);
+        console.log('iterator', i);
+        console.log('text', this.txt + '\n***');
+
         setTimeout(function() {
-        that.tick();
+            that.tick();
         }, delta);
     };
 
@@ -51,6 +78,5 @@ var TxtType = function(el, toRotate, period) {
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
